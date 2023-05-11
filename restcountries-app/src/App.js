276431dd9fed7975abc63766700da6e0baf79567
James Paterson <james@MASTUDENT-James-Paterson-C17GMB0JQ6L4.local>
2023-05-11 21:22:18 +0100

    added basic search bar and comments

--- restcountries-app/src/App.js
+++ restcountries-app/src/App.js
@@ -4,6 +4,24 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [query, setQuery] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://restcountries.com/v3.1/name/${query}`)
+      .then(response => response.json())
+      .then(data => {
+        // handle the response data
+      })
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
   
 }
 
