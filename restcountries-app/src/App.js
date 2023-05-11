adea58366f33e690bbf7f26729be754ab9c187f5
James Paterson <james@MASTUDENT-James-Paterson-C17GMB0JQ6L4.local>
2023-05-11 21:58:20 +0100

    added function and more css to whole page

--- restcountries-app/src/App.js
+++ restcountries-app/src/App.js
@@ -12,37 +12,36 @@ function App() {
     fetch(`https://restcountries.com/v3.1/name/${query}`)
       .then(response => response.json())
       .then(data => {
-        // handles the response data
-        const countries = data.map(country => ({
-          name: country.name.common,
-          population: country.population
-          //Initial search for name and population
-        }));
-        setResponse(countries)
-        console.log(countries); // To see if there's any issues and to initially see if it's working without having to show it
-        
+        setResponse(data);
       })
-      .catch(error => console.error(error));
-
+      // removed excess and returned data
   }
+  
   return (
     <div>
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
-      <button type="submit">Search</button>
-      {/* Added a search bar form with a button */}
-    </form>
+      <header>
+      <h1>RestCountriesApp</h1>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
+        <button type="submit">Search</button>
+        {/* Added a search bar form with a button */}
+      </form>
 
-    {response && (
-        <div>
-          {response.map(country => (
-            <div key={country.name}>
-              <h2>{country.name}</h2>
-              <p>Population: {country.population}</p>
-            </div>
-          ))}
-        </div>
-      )}
+      {response && (
+  <div>
+    {response.map(country => (
+      <div key={country.name.common} className="country">
+        <h2>{country.name.common}</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Region: {country.region}</p>
+        <p>Population: {country.population}</p>
+      </div>
+    ))}
+  </div>
+)}
+
+
   </div>   
   )
 
